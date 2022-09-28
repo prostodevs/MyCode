42fd7b4a6c39a0cb5c94c4e1b8aea7d1d2c8b06e
prostodevs <prostodevs@gmail.com>
2022-09-28 19:45:15 +0000

    Py challenge #14

---
+++ swChallenge/pychallenge.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3
+"""Alta3 training | Justin S
+   Py Challenge"""
+
+
+def main():
+    
+    # prompts for user name
+    name_input = input("Please enter your name: ")
+    # print("Your name is: " + name_input)
+
+    # prompt for the day of the week
+    day_input = input("Enter the day of the week: ")
+
+    # output statement using name and day
+    print("Hello, " + name_input + "! Happy " + day_input + "!")
+main()
